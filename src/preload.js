8933585d4f7c78b6fb2991a50820d2e09d717357
dustywill <99150368+dustywill@users.noreply.github.com>
2025-06-30 10:47:19 -0500

    feat: log non-serializable ipc data

--- src/preload.js
+++ src/preload.js
@@ -1,27 +1,53 @@
 const { contextBridge, ipcRenderer } = require("electron");
 
+// Utility to log when an argument cannot be cloned/serialized
+const logIfNotSerializable = (label, value) => {
+  try {
+    // structuredClone will throw if value cannot be cloned
+    structuredClone(value);
+  } catch (err) {
+    console.warn(`[preload] ${label} is not serializable`, value);
+  }
+};
+
+const safeInvoke = async (channel, ...args) => {
+  args.forEach((arg, idx) =>
+    logIfNotSerializable(`arg[${idx}] for ${channel}`, arg),
+  );
+  try {
+    return await ipcRenderer.invoke(channel, ...args);
+  } catch (err) {
+    console.error(`[preload] ipcRenderer.invoke failed for ${channel}`, err);
+    throw err;
+  }
+};
+
 // Expose protected methods that allow the renderer process to use
 // the ipcRenderer without exposing the entire object
-contextBridge.exposeInMainWorld("electronAPI", {
-  loadSampleData: (data) => ipcRenderer.invoke("load-sample-data", data),
+const electronAPI = {
+  loadSampleData: (data) => safeInvoke("load-sample-data", data),
 
   // File system operations - you'll need to implement these in main process
   readFile: (filePath, options) =>
-    ipcRenderer.invoke("fs-read-file", filePath, options),
+    safeInvoke("fs-read-file", filePath, options),
   writeFile: (filePath, data) =>
-    ipcRenderer.invoke("fs-write-file", filePath, data),
-  mkdir: (dirPath, options) => ipcRenderer.invoke("fs-mkdir", dirPath, options),
+    safeInvoke("fs-write-file", filePath, data),
+  mkdir: (dirPath, options) => safeInvoke("fs-mkdir", dirPath, options),
   readdir: (dirPath, options) =>
-    ipcRenderer.invoke("fs-readdir", dirPath, options),
+    safeInvoke("fs-readdir", dirPath, options),
 
   // Path operations
-  join: (...paths) => ipcRenderer.invoke("path-join", paths),
+  join: (...paths) => safeInvoke("path-join", paths),
 
   // Other utilities
-  getCwd: () => ipcRenderer.invoke("get-cwd"),
-});
+  getCwd: () => safeInvoke("get-cwd"),
+};
+logIfNotSerializable("electronAPI", electronAPI);
+contextBridge.exposeInMainWorld("electronAPI", electronAPI);
 
 // For backward compatibility with your existing code
-contextBridge.exposeInMainWorld("ipcRenderer", {
-  invoke: (channel, data) => ipcRenderer.invoke(channel, data),
-});
+const exposedIpc = {
+  invoke: (channel, data) => safeInvoke(channel, data),
+};
+logIfNotSerializable("ipcRenderer", exposedIpc);
+contextBridge.exposeInMainWorld("ipcRenderer", exposedIpc);
